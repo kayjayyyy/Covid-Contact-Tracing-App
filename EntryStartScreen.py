dc54a3a9e140731ab6d3b1610c186fd38a552153
unknown <templanzatintin@gmail.com>
2023-07-24 18:32:47 +0800

    create class and title for new window and form

--- EntryStartScreen.py
+++ EntryStartScreen.py
@@ -5,3 +5,16 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 import csv
+
+class StartScreen(tk.Tk):
+
+    def init(self):
+        super().__init__()
+
+        self.title("Covid Contact Tracing")
+        self.geometry("500x800")
+        self.configure(bg="#E3DAFF")
+
+        title = tk.Label(self, text="COVID CONTACT TRACING FORM", font=("Source Sans Pro", 15, "bold"), bg="#E4CCB4")
+        title.place(x=83, y=20)
+        
