c54858cafa1c0000e19d33e651ef04c98398e497
unknown <templanzatintin@gmail.com>
2023-07-24 22:13:39 +0800

    symptoms label

--- EntryStartScreen.py
+++ EntryStartScreen.py
@@ -103,8 +103,8 @@ class StartScreen(tk.Tk):
         no_button_3.place(x=370, y=345)
         
         # Symptoms no. 4 Cough or sore throat
-        question_4_label = tk.Label(self, text="Cough or sore throat?", font=("Source Sans Pro", 11, "bold"), background="#E3DAFF")
-        question_4_label.place(x=80, y=375)
+        symptoms_4_label = tk.Label(self, text="Cough or sore throat?", font=("Source Sans Pro", 11, "bold"), background="#E3DAFF")
+        symptoms_4_label.place(x=80, y=375)
 
         yes_button_4 = ttk.Radiobutton(self, variable=self.qs4, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_button_4.place(x=280, y=375)
@@ -113,8 +113,8 @@ class StartScreen(tk.Tk):
         no_button_4.place(x=370, y=375)
         
         # Symptoms no. 5 Shortness of Breath
-        question_5_label = tk.Label(self, text="Shortness of breath?", font=("Source Sans Pro", 11, "bold"), background="#E3DAFF")
-        question_5_label.place(x=80, y=405)
+        symptoms_5_label = tk.Label(self, text="Shortness of breath?", font=("Source Sans Pro", 11, "bold"), background="#E3DAFF")
+        symptoms_5_label.place(x=80, y=405)
 
         yes_button_5 = ttk.Radiobutton(self, variable=self.qs5, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_button_5.place(x=280, y=405)
