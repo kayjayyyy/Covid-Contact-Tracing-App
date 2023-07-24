a04fcb6498dab30a92d9913b46cdcaf8fc3257d1
unknown <templanzatintin@gmail.com>
2023-07-25 02:02:08 +0800

    create class main screen and create window for main screen

--- ContactTracingApp.py
+++ ContactTracingApp.py
@@ -4,3 +4,13 @@
 
 import tkinter as tk
 from EntryStartScreen import StartScreen
+
+class MainScreen(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("Contact Tracing App")
+        self.geometry("500x780")
+        self.configure(bg="#E3DAFF")
+        self.pack_propagate(0)
