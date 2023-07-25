944969394b7a0bbd5c8e0b1fcac22ee35f6be227
unknown <templanzatintin@gmail.com>
2023-07-26 03:58:28 +0800

    add instruction label

--- ContactTracingApp.py
+++ ContactTracingApp.py
@@ -17,3 +17,17 @@ class MainScreen(tk.Tk):
         
         title = tk.Label(self, text="Covid-19 Contact Tracing App", font=("Source Sans Pro", 20, "bold"), bg="#E3DAFF")
         title.place(x=48, y=150)
+        
+        # Instruction
+        instruction = tk.Label(self, text="If you are either diagnosed with Coronavirus or experiencing", font=("Source Sans Pro", 11), background="#E3DAFF")
+        instruction.place(x=53, y=250)
+        
+        instruction = tk.Label(self, text="Coronavirus symptoms, please provide us with your contact information.", font=("Source Sans Pro", 11), background="#E3DAFF")
+        instruction.place(x=18, y=270)
+        
+        instruction_2 = tk.Label(self, text="And you can also check here any interactions a person may have", font=("Source Sans Pro", 11), background="#E3DAFF")
+        instruction_2.place(x=40, y=290)
+        
+        instruction_2 = tk.Label(self, text="had with somebody who might be a COVID-19 virus carrier.", font=("Source Sans Pro", 11), background="#E3DAFF")
+        instruction_2.place(x=53, y=310)
+        
